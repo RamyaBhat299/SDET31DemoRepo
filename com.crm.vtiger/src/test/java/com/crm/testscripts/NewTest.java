7b473a4420b23f974c2750c4f1e3ae0b73f533f5
Ramya <Ramya@LAPTOP-FJFDJBKU>
2022-03-23 17:36:39 +0530

    second repo

---
+++ com.crm.vtiger/src/test/java/com/crm/testscripts/NewTest.java
@@ -0,0 +1,13 @@
+package com.crm.testscripts;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	@Test
+	public void newRepo()
+	{
+		System.out.println("new repo printing");
+	}
+
+}
